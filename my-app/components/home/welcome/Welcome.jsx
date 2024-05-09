bdc9c2934a4424cc6ccd9740bf5440627bf65b5f
tomiwakukoyi <tomiwakukoyiweb@gmail.com>
2024-05-09 12:50:16 +0100

    Setup

---
+++ my-app/components/home/welcome/Welcome.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList,
+} from "react-native";
+import { useRouter } from "expo-router";
+import styles from "./welcome.style";
+import { icons, SIZES } from "../../../constants";
+
+const Welcome = () => {
+  const router = useRouter();
+  return (
+    <View>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Tomiwa</Text>
+        <Text style={styles.welcomeMessage}>Lets get you jobbed</Text>
+      </View>
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput
+            style={styles.searchInput}
+            placeholder="Search jobs..."
+            value=""
+            onChange={() => {}}
+          />
+        </View>
+
+        <TouchableOpacity style={styles.searchBtn}>
+          <Image
+            source={icons.search}
+            resizeMode="contain"
+            style={styles.searchBtnImage}
+          />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default Welcome;
