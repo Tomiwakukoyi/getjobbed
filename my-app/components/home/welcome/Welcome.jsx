20701a3c972be54315c22961f37791c6d8c52851
tomiwakukoyi <tomiwakukoyiweb@gmail.com>
2024-05-13 15:58:25 +0100

    ...

--- my-app/components/home/welcome/Welcome.jsx
+++ my-app/components/home/welcome/Welcome.jsx
@@ -11,8 +11,12 @@ import { useRouter } from "expo-router";
 import styles from "./welcome.style";
 import { icons, SIZES } from "../../../constants";
 
+const jobTypes = ["Full-time", "Part-time", "Contract"];
+
 const Welcome = () => {
   const router = useRouter();
+
+  const [activeJobType, setActiveJobType] = useState("Full-time");
   return (
     <View>
       <View style={styles.container}>
@@ -36,6 +40,25 @@ const Welcome = () => {
             style={styles.searchBtnImage}
           />
         </TouchableOpacity>
+      </View>
+      <View style={styles.tabsContainer}>
+        <FlatList
+          data={jobTypes}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={styles.tab(activeJobType, item)}
+              onPress={() => {
+                setActiveJobType(item);
+                router.push(`/search/${item}`);
+              }}
+            >
+              <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
+            </TouchableOpacity>
+          )}
+          keyExtractor={item =>item}
+          contentContainerStyle={{columnGap:SIZES.small}}
+          horizontal
+        />
       </View>
     </View>
   );
