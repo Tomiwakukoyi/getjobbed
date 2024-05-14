ad2a8a1c14dc0215de5012a2c6b37280bc2bc346
tomiwakukoyi <tomiwakukoyiweb@gmail.com>
2024-05-14 17:05:10 +0100

    almost done

--- my-app/app/index.js
+++ my-app/app/index.js
@@ -11,6 +11,8 @@ import {
 
 const Home = () => {
   const router = useRouter();
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
       <Stack.Screen
@@ -33,7 +35,15 @@ const Home = () => {
             padding: SIZES.medium,
           }}
         >
-          <Welcome />
+          <Welcome
+            searchTerm={searchTerm}
+            setSearchTerm={setSearchTerm}
+            handleClick={() => {
+              if (searchTerm) {
+                router.push(`/search/${searchTerm}`);
+              }
+            }}
+          />
 
           <Popularjobs />
 
